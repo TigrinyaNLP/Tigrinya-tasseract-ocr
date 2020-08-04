f621e046f2f96d5656222d3545409cceecc700df
lomitec <bin_mil@yahoo.com>
2020-08-03 18:29:55 -0600

    geez fonts for tesseract 4.0 training

--- bin/train_from_scrach.sh
+++ bin/train_from_scrach.sh
@@ -12,10 +12,10 @@ wget https://raw.githubusercontent.com/tesseract-ocr/langdata/master/Ethiopic.xh
 wget https://raw.githubusercontent.com/tesseract-ocr/langdata_lstm/master/Latin.xheights
 mkdir tir
 cd tir
-wget https://raw.githubusercontent.com/tesseract-ocr/langdata/master/tir/tir.training_text
+wget https://raw.githubusercontent.com/TigrinyaNLP/Tigrinya-tasseract-ocr/master/resources/langdata/tir/tir.training_text
+wget https://raw.githubusercontent.com/TigrinyaNLP/Tigrinya-tasseract-ocr/master/resources/langdata/tir/tir.wordlist
 wget https://raw.githubusercontent.com/tesseract-ocr/langdata/master/tir/tir.punc
 wget https://raw.githubusercontent.com/tesseract-ocr/langdata/master/tir/tir.numbers
-wget https://raw.githubusercontent.com/tesseract-ocr/langdata/master/tir/tir.wordlist
 
 cd ~/tesstutorial
 git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git
@@ -27,3 +27,11 @@ cd best
 wget https://github.com/tesseract-ocr/tessdata_best/raw/master/tir.traineddata
 wget https://github.com/tesseract-ocr/tessdata_best/raw/master/heb.traineddata
 wget https://github.com/tesseract-ocr/tessdata_best/raw/master/chi_sim.traineddata
+
+tesstrain.sh --fonts_dir langdata/fonts --lang tir --linedata_only  --noextract_font_properties --langdata_dir langdata  --tessdata_dir tesseract/tessdata --output_dir ~/tesstutorial/tirtrain --fontlist "Abyssinica SIL"
+
+tesstrain.sh --fonts_dir langdata/fonts --lang tir --linedata_only  --noextract_font_properties --langdata_dir langdata  --tessdata_dir tesseract/tessdata --fontlist "Abyssinica SIL" --output_dir ~/tesstutorial/tireval
+
+mkdir -p ~/tesstutorial/tiroutput
+mkdir -p ~/tesstutorial/tiroutput/base
+lstmtraining --debug_interval 100   --traineddata ~/tesstutorial/tirtrain/tir/tir.traineddata   --net_spec '[1,36,0,1 Ct3,3,16 Mp3,3 Lfys48 Lfx96 Lrx96 Lfx256 O1c352]'  --model_output ~/tesstutorial/tiroutput/base --learning_rate 20e-4   --train_listfile ~/tesstutorial/tirtrain/tir.training_files.txt   --eval_listfile ~/tesstutorial/tireval/tir.training_files.txt   --max_iterations 5000 &>~/tesstutorial/tiroutput/basetrain.log
