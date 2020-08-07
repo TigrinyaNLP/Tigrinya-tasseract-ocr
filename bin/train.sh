2b90bedf1f4ef96b2873d45a2361176349ba7d3d
Biniam G <33637695+biniamgebremichael@users.noreply.github.com>
2020-08-06 18:38:34 -0600

    Update train.sh

--- bin/train.sh
+++ bin/train.sh
@@ -1,4 +1,15 @@
-lstmtraining   --traineddata ~/build/tirtrain/tir/tir.traineddata  \
+
+if [ "$1" = "scratch" ]; then
+    TIR_TESSDATA="~/build/tirtrain/tir/tir.traineddata"
+else if [ "$1" = "best" ]; then
+    TIR_TESSDATA="~/build/tesseract/tessdata/best"
+else
+    echo "Usage: train.sh [ scratch | best ]  -- start training from already existing best tir.tessdata or train from scratch from the empty one built earlier"
+    exit 1;
+fi
+
+mkdir ~/build/tiroutput
+lstmtraining   --traineddata "$TIR_TESSDATA"  \
                --net_spec '[1,36,0,1Ct3,3,16Mp3,3Lfys48Lfx96Lrx96Lfx128O1c1]' \
                --learning_rate 0.001 \
                --momentum  0.5 \
@@ -7,3 +18,9 @@ lstmtraining   --traineddata ~/build/tirtrain/tir/tir.traineddata  \
                --model_output ~/build/tiroutput/base \
                --train_listfile ~/build/tirtrain/tir.training_files.txt \
                --eval_listfile ~/build/tireval/tir.training_files.txt
+
+
+#lstmtraining --stop_training  --continue_from ~/tesstutorial/tiroutput/base_checkpoint  --traineddata ~/tesstutorial/tirtrain/tir/tir.traineddata   --model_output ~/tesstutorial/tiroutput/tir.traineddata
+
+# TEST
+# tesseract  --tessdata-dir ~/Tigrinya-tasseract-ocr/tiroutput/ -l tir Tigrinya-tasseract-ocr/resources/test/tif/tir.bible_01.tif  tir.bible_01
